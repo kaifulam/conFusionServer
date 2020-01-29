2f6be7ba0a0a7cddbad798681cfe90c13b436b59
Kaifu Lam <kaifulam@u.washington.edu>
2020-01-29 03:44:21 -0800

    hw4.v1

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '167839407827345', //'Your Client App ID',
-        clientSecret: 'f234e1ef989f76225654e9235a44e390', //'Your Client App Secret'
+        clientId: , //'Your Client App ID',
+        clientSecret: //'Your Client App Secret'
     }
 }
